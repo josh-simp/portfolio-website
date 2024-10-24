35b122f0c43e2daa9da3bc88a4c3eb6b429ffd81
josh-simp <61167175+josh-simp@users.noreply.github.com>
2024-10-24 10:29:28 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2"> 
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="pb-16 text-6x1 font-thin tracking-tight lg:mt-16 lg:text-8xl">Joshua Simpson</h1>
+                    <h1 className="pb-16 text-5xl sm:text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Joshua Simpson</h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">FrontEnd Developer</span>
                     <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
                 </div>
