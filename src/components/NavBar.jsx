f10ee2d57e41da6dd676036294fb8f24580feaad
josh-simp <61167175+josh-simp@users.noreply.github.com>
2024-10-24 10:35:53 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/joshua-i-simpson" target="_blank"><FaLinkedin/></a>
-            <a href="https://github.com/josh-simp/Josh.git" target="_blank"><FaGithub/></a>
+            <a href="https://github.com/josh-simp/portfolio-website" target="_blank"><FaGithub/></a>
             <a href="https://www.figma.com/design/jXkHwjVAKE8SLAjdmdiPdQ/Portfolio?node-id=0-1&t=dJgDK5hqpeyV6Sh8-1" target="_blank"><FaFigma/></a>
             <a href="https://www.instagram.com/josh_simpson81/profilecard/?igsh=MXA2bXZ0NW1hbmRsbA==" target="_blank"><FaInstagram/></a>
         </div>
